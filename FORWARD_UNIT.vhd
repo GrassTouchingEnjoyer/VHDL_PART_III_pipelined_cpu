e8b7866e788b2948369c63f2dfd9a86db33a3503
CenoByte <147980342+GrassTouchingEnjoyer@users.noreply.github.com>
2024-06-20 14:36:56 +0300

    Add files via upload
    built a pipeline with no hazards.

---
+++ FORWARD_UNIT.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:49:32 06/17/2024 
+-- Design Name: 
+-- Module Name:    FORWARD_UNIT - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FORWARD_UNIT is
+end FORWARD_UNIT;
+
+architecture Behavioral of FORWARD_UNIT is
+
+begin
+
+
+end Behavioral;
+
